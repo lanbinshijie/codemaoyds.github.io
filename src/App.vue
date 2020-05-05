1852be89b545f084b454985c650d3c1f2d2fc865
ydszq <123632704@qq.com>
2020-05-05 22:47:34 +0800

    第一次变更

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <ul class="nav_ul">
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
